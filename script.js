0e7284714083a383e3c00d3cbe2c97aaaad60efd
Marisa Sales <73047662+marisasales@users.noreply.github.com>
2023-01-02 00:24:14 -0300

    feat: add event listener and update direction

--- script.js
+++ script.js
@@ -40,6 +40,15 @@ function createMovement() {
   snake.unshift(newHead)
 }
 
+function directionUpdate(event) {
+  if (event.keyCode == 37 && direction != "right") direction = "left"
+  if (event.keyCode == 38 && direction != "down") direction = "up"
+  if (event.keyCode == 39 && direction != "left") direction = "right"
+  if (event.keyCode == 40 && direction != "up") direction = "down"
+  }
+  
+  addEventListener('keydown', directionUpdate)
+
 function startGame() {
   createBG()
   createSnake()
